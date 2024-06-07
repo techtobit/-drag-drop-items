574da1db81af7dbe445956ab37b56dc7f0d39ce0
techtobit <nurthern2016@gmail.com>
2024-06-07 21:14:33 +0600

    js file added and linked

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('container');
+const nodes = Array.from(document.getElementById('node'));
+
+
+let activeNode = null;
+let offsetX, offsetY;
+
+nodes.forEach(nodes => {
+	
+})
